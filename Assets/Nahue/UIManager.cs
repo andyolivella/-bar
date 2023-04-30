e50da3767703ea0d4a466199ce2391cfc6625551
NahuelHawk <nahuel36gg@gmail.com>
2023-04-30 15:33:12 -0300

    empezando levelmanager y ordenando un poco todo

--- Assets/Nahue/UIManager.cs
+++ Assets/Nahue/UIManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 public class UIManager : MonoBehaviour
 {
     [SerializeField]Image healthImg;
@@ -9,12 +10,19 @@ public class UIManager : MonoBehaviour
     Health health;
     private int maxHealth;
     SpecialAttack special;
+    [SerializeField] GameObject GO_gameobj;
     // Start is called before the first frame update
     void Start()
     {
         health = GameObject.FindGameObjectWithTag("Player").GetComponent<Health>();
         maxHealth = health.DefaultHealth;
         special = GameObject.FindGameObjectWithTag("Player").GetComponent<SpecialAttack>();
+    }
+
+    public void HideGoText()
+    {
+        GO_gameobj.SetActive(false);
+    
     }
 
     // Update is called once per frame
