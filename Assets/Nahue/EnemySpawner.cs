753e2f20ecb01820afdb2cb70383a9ab3e0ebb84
NahuelHawk <nahuel36gg@gmail.com>
2023-05-01 04:30:05 -0300

    object pool separado del spawner

--- Assets/Nahue/EnemySpawner.cs
+++ Assets/Nahue/EnemySpawner.cs
@@ -5,107 +5,39 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] Transform[] spawnPoints;
-    [SerializeField] GameObject enemyMeleePrefab;
-    [SerializeField] GameObject enemyShootPrefab;
-    [SerializeField] int maxCuantityAtSameTime = 30;
-    private List<GameObject> meleeEnemys;
-    private List<GameObject> shotEnemys;
     [SerializeField] int[] spawnCuantity;
-    private int currentWave;
-    private float currentTime;
+    private int currentWave = 0;
+    private float currentTime = 0;
     [SerializeField] float waveDuration = 10;
     private int currentWaveSpawnedCuantity = 0;
     private int currentWaveRecycledCuantity = 0;
     [SerializeField] LevelManager level_manager;
     private bool canSpawn = false;
+    [SerializeField] ObjectPool[] enemysPools;
     // Start is called before the first frame update
-    void Start()
-    {
-        currentWaveSpawnedCuantity = 0;
-        meleeEnemys = new List<GameObject>();
-        shotEnemys = new List<GameObject>();
-        currentWave = 0;
-        for (int i = 0; i < maxCuantityAtSameTime; i++)
-        {
-            meleeEnemys.Add(Instantiate(enemyMeleePrefab));
-            shotEnemys.Add(Instantiate(enemyShootPrefab));
-        }
-        for (int i = 0; i < maxCuantityAtSameTime; i++)
-        {
-            meleeEnemys[i].SetActive(false);
-            shotEnemys[i].SetActive(false);
-        }
-    }
-
     public void recycleEnemy(GameObject enemy) 
     {
-        if(meleeEnemys.Contains(enemy))
-        {
-            for (int i = 0; i < maxCuantityAtSameTime; i++)
-            {
-                if (meleeEnemys[i] == enemy)
-                { 
-                    meleeEnemys[i].SetActive(false);
-                    currentWaveRecycledCuantity++;
-                }
-            }
-        }
-        if(shotEnemys.Contains(enemy))
-        {
-            for (int i = 0; i < maxCuantityAtSameTime; i++)
-            {
-                if (shotEnemys[i] == enemy)
-                { 
-                    shotEnemys[i].SetActive(false);
-                    currentWaveRecycledCuantity++;
-                }
-            }
-        }
+        foreach (ObjectPool pool in enemysPools)
+            pool.Release(enemy);
+        currentWaveRecycledCuantity++;
     }
 
     void SpawnEnemy() 
     {
-        int ranEnemy = Random.Range(0, 2);
-        GameObject enemy = null;
-        if (ranEnemy == 0)
-        {
-            foreach (GameObject meleeEnemy in meleeEnemys)
-            {
-                if (!meleeEnemy.activeInHierarchy)
-                {
-                    enemy = meleeEnemy;
-                    break;
-                }
-            }
-        }
-        else
-        {
-            foreach (GameObject shootEnemy in shotEnemys)
-            {
-                if (!shootEnemy.activeInHierarchy)
-                {
-                    enemy = shootEnemy;
-                    break;
-                }
-            }
-        }
-
-        if (enemy != null)
-        { 
-            enemy.SetActive(true);
-            int ranSpawnPoint = Random.Range(0, spawnPoints[currentWave].childCount);
-            enemy.transform.parent = spawnPoints[currentWave].GetChild(ranSpawnPoint);
-            enemy.transform.localPosition = Vector3.zero;
-            enemy.GetComponent<Health>().spawner = this;
-            currentWaveSpawnedCuantity++;
-            return;
-        }
+        int ranEnemy = Random.Range(0, enemysPools.Length);
+        GameObject enemy = enemysPools[ranEnemy].Get();
+        int ranSpawnPoint = Random.Range(0, spawnPoints[currentWave].childCount);
+        enemy.transform.parent = spawnPoints[currentWave].GetChild(ranSpawnPoint);
+        enemy.transform.localPosition = Vector3.zero;
+        enemy.GetComponent<Health>().spawner = this;
+        currentWaveSpawnedCuantity++;
     }
 
     public void Activate() {
         canSpawn = true;
         currentTime = 0;
-
+        currentWaveSpawnedCuantity = 0;
+        currentWaveRecycledCuantity = 0;
     }
 
     public void Deactivate()
@@ -127,10 +59,7 @@ public class EnemySpawner : MonoBehaviour
             if (currentWaveRecycledCuantity >= spawnCuantity[currentWave])
                 {
                    level_manager.SetMoving();
-                   currentTime = 0;
                    currentWave++;
-                   currentWaveSpawnedCuantity = 0;
-                   currentWaveRecycledCuantity = 0;
                 }
         }
     }
---
+++ Assets/Nahue/ObjectPool.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    List<GameObject> pool;
+    [SerializeField] GameObject prefab;
+    [SerializeField] int maxCuantityAtSameTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pool = new List<GameObject>();
+        for (int i = 0; i < maxCuantityAtSameTime; i++)
+        {
+            pool.Add(Instantiate(prefab));
+        }
+        for (int i = 0; i < pool.Count; i++)
+        {
+            pool[i].SetActive(false);
+        }
+    }
+
+    public void Release(GameObject objectToRelease)
+    {
+        if (pool.Contains(objectToRelease))
+            objectToRelease.SetActive(false);
+    }
+
+    public GameObject Get()
+    {
+        foreach (GameObject gobj in pool)
+        {
+            if (!gobj.activeInHierarchy)
+            {
+                gobj.SetActive(true);
+                return gobj;
+            }
+        }
+        GameObject newObj = Instantiate(prefab);
+        pool.Add(newObj);
+        return newObj;
+    }
+
+
+}
