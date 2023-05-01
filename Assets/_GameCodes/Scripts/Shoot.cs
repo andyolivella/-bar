f8b558dd99a7f958bb64e49ffa458d24c37aa43c
NahuelHawk <nahuel36gg@gmail.com>
2023-05-01 04:41:59 -0300

    que las balas de los enemigos sean con object pool tambien

--- Assets/_GameCodes/Scripts/Shoot.cs
+++ Assets/_GameCodes/Scripts/Shoot.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Shoot : MonoBehaviour
 {
-    public GameObject bulletPrefab;
     public Transform bulletOrigin;
     public float bulletForce = 5;
+    public ObjectPool bulletsPool;
     // Start is called before the first frame update
     public void ShootTo( GameObject objetive)
     {
-        GameObject gobj = Instantiate<GameObject>(bulletPrefab);
+        GameObject gobj = bulletsPool.Get();
         gobj.transform.position = bulletOrigin.position;
         Rigidbody rb = gobj.GetComponent<Rigidbody>();
+        rb.velocity = Vector3.zero;
         rb.AddForce((objetive.transform.position - gobj.transform.position).normalized * bulletForce, ForceMode.Impulse);
     }
 
